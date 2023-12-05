4ffa56bfabfcdb266fa2a92697029a0104b06ff6
SanjarbekAI <sanjarbekwork@gmail.com>
2023-12-05 13:42:59 +0500

    third

--- bot.py
+++ bot.py
@@ -4,11 +4,12 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemo
 from aiogram.dispatcher.filters import BoundFilter
 
 API_TOKEN = '6432695102:AAFgw8IcBv9PYLLG98oCaHRYMY3NMhwJ0BQ'
+# API_TOKEN = 'another bot token'
 
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
-ADMINS = [5374970910]
+ADMINS = [5374970910,837251184,1233658877]
 
 generate_link_button = ReplyKeyboardMarkup(
     keyboard=[[
@@ -32,7 +33,7 @@ async def admin_start_handler(message: types.Message):
 @dp.message_handler(IsPrivate(), chat_id=ADMINS, text="🔗 Link olish")
 async def create_invite_link(message: types.Message):
     try:
-        # chat_id = -1002140470690
+        # chat_id = -1002140470690 # test version
         chat_id = -1001922060151
         result = await bot.create_chat_invite_link(chat_id=chat_id, member_limit=1)
         invite_link = result.invite_link
